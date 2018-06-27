6aea1bca0f9385e6a4876cba04389a1607b06f4b
Brian <39497182+bpalowski@users.noreply.github.com>
2018-06-26 19:08:20 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,31 +1,45 @@
+
 using System;
 
 namespace Practice
 {
-class Program
-{
-public static void Main()
+  class Program
   {
-    int runningTotal = 0;
-    bool keepGoing = true;
-
-    while(keepGoing)
+    public static void Main()
     {
-      Console.Write("Enter how many minutes your exercised or type 'quit' to exit: ");
-      string entry = Console.ReadLine();
+      int runningTotal = 0;
+      bool keepGoing = true;
 
-      if(entry == "quit")
+      while(keepGoing)
       {
-        keepGoing = false;
-      }
-      else{
-      int minutes = int.Parse(entry);
-      runningTotal = runningTotal + minutes;
+        Console.Write("Enter how many minutes your exercised or type 'quit' to exit: ");
+        string entry = Console.ReadLine();
 
-      Console.WriteLine("You've entered " + runningTotal + " minutes");
+        if(entry == "quit")
+        {
+          keepGoing = false;
+        }
+        else{
+          int minutes = int.Parse(entry);
+
+          if(minutes <= 10){
+            Console.WriteLine("Better than nothing");
+          }
+          else if( minutes <= 30){
+            Console.WriteLine("Way to go");
+          }
+          else if( minutes <= 60){
+            Console.WriteLine("Keep it up");
+          }
+          else{
+            Console.WriteLine("You are awsome");
+          }
+          runningTotal = runningTotal + minutes;
+
+          Console.WriteLine("You've entered " + runningTotal + " minutes");
+        }
+      }
+      Console.WriteLine("goodbye");
     }
-    }
-    Console.WriteLine("goodbye");
   }
-}
 }
